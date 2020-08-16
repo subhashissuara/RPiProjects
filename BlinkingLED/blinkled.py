9ec1f2e069f807d86ae2125cc7e273a97eda9c44
Subhashis Suara <subhashis.suara999@gmail.com>
2020-08-16 13:40:40 +0530

    Updated Blinking LED Project

--- BlinkingLED/blinkled.py
+++ BlinkingLED/blinkled.py
@@ -4,8 +4,11 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD) # Use board pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 
-while True:
-    GPIO.output(8, GPIO.HIGH) # Turn on
-    sleep(1)
-    GPIO.output(8, GPIO.LOW) # Turn off
-    sleep(1)
+try:
+    while True:
+        GPIO.output(8, GPIO.HIGH) # Turn on
+        sleep(1)
+        GPIO.output(8, GPIO.LOW) # Turn off
+        sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
